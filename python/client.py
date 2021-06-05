6f8a2b1a27ac907b8d653fc9e5dcd713712cef30
Marco Parisi <mparisi@Marcos-MacBook-Pro.local>
2021-06-05 09:05:09 +0100

    first commit

---
+++ python/client.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+
+class Client(object):
+    def __init__(self):
+        self.name = ''
+        self.surname = ''
+        self.city = ''
+        self.birthday = ''
+        self.fiscal_code = ''
